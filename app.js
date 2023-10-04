e78f3128aa3ad9438cd9715f3701fed30cf4243d
Joshckh <105426491+Joshckh@users.noreply.github.com>
2023-10-04 15:20:14 +0800

    added checkWin logic

--- app.js
+++ app.js
@@ -1,33 +1,72 @@
 const gameBoard = (() => {
     let board = [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
-    ]
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
 
     const clearBoard = () => {
         board = [
-            [0,0,0],
-            [0,0,0],
-            [0,0,0]
-        ]
-    }
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
+    };
 
     const placeX = (x, y) => {
         if (board[x][y] === 0) {
             board[x][y] = "X";
         } else {
             console.log("Invalid move for X.");
+        }
+    };
 
-  const placeO = (x, y) => {
+    const placeO = (x, y) => {
         if (board[x][y] === 0) {
             board[x][y] = "O";
         } else {
             console.log("Invalid move for O.");
+        }
+    };
 
-    return{
+    const checkWin = () => {
+
+        for (let i = 0; i < 3; i++) {
+            if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== 0) {
+               
+            }
+        }
+
+        for (let i = 0; i < 3; i++) {
+            if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== 0) {
+                
+            }
+        }
+
+        if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== 0) {
+       
+        }
+
+        if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== 0) {
+           
+        }
+
+        return ;
+    };
+
+        const checkDraw = () =>{
+
+        }
+
+        const startGame = () =>{
+            
+        }
+
+    return {
+        board,
         clearBoard,
         placeX,
         placeO,
-    }
-})()
+        checkWin,
+    };
+})();
