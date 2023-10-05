ab892409214f0d8be21a6b90580cd4112690404c
Joshckh <105426491+Joshckh@users.noreply.github.com>
2023-10-05 15:44:36 +0800

    fixed some JS logic and added basic html and css

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const gameBoard = (() => {
+const player = (name,sign) => {
+    return {name,sign}
+}
+
+
+const game = (() => {
     let board = [
         [0, 0, 0],
         [0, 0, 0],
@@ -33,34 +38,27 @@ const gameBoard = (() => {
 
         for (let i = 0; i < 3; i++) {
             if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== 0) {
-               
+               return console.log("Winner")
             }
         }
 
         for (let i = 0; i < 3; i++) {
             if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== 0) {
-                
+                return console.log("Winner")
             }
         }
 
         if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== 0) {
-       
+            return console.log("Winner")
         }
 
         if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== 0) {
-           
+            return console.log("Winner")
         }
 
-        return ;
+        console.log(board);
     };
 
-        const checkDraw = () =>{
-
-        }
-
-        const startGame = () =>{
-            
-        }
 
     return {
         board,
@@ -70,3 +68,8 @@ const gameBoard = (() => {
         checkWin,
     };
 })();
+
+const display = (() =>{
+   
+})()
+
