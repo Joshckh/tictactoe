316bef88abc8c154e817c968211a8afdfca2d481
Joshckh <105426491+Joshckh@users.noreply.github.com>
2023-10-27 11:31:06 +0800

    Bot V.1

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const player = (name,sign) => {
-    return {name,sign}
-}
-
-
 const game = (() => {
     let board = [
         [0, 0, 0],
         [0, 0, 0],
-        [0, 0, 0]
+        [0, 0, 0],
     ];
 
     const clearBoard = () => {
@@ -17,7 +12,6 @@ const game = (() => {
             }
         }
     };
-    
 
     const placeX = (x, y) => {
         if (board[x][y] === 0) {
@@ -37,7 +31,7 @@ const game = (() => {
 
     const checkDraw = () => {
         let isDraw = true;
-    
+
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 if (board[i][j] === 0) {
@@ -46,16 +40,15 @@ const game = (() => {
                 }
             }
         }
-    
+
         if (isDraw) {
             alert("It's a draw!");
             clearBoard();
-            display.start()
+            display.start();
         }
     };
 
     const checkWin = () => {
-
         for (let i = 0; i < 3; i++) {
             if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== 0) {
                 alert(`${board[i][0]} is the Winner`)
@@ -84,14 +77,9 @@ const game = (() => {
             display.start()
         }
 
+
         checkDraw();
-       
-
-        console.log(board);
     };
-
-   
-
 
     return {
         placeX,
@@ -104,95 +92,90 @@ const game = (() => {
 
 const display = (() => {
     const tiles = document.querySelectorAll(".tile");
-    let xTurn = true
-    let botMatch = false
-    
+    let xTurn = true;
+    let botMatch = false;
 
     const clickHandler = (e) => {
-        const clickedTile = e.target; 
+        const clickedTile = e.target;
         const row = parseInt(clickedTile.dataset.row);
         const col = parseInt(clickedTile.dataset.col);
-
-        if (botMatch === false){
-
-            if (xTurn === true) {
-                game.placeX(row, col);
-                clickedTile.classList.add("xIcon");
-                xTurn = false;
-                game.checkWin();
-            } else {
-                game.placeO(row, col);
-                clickedTile.classList.add("oIcon");
-                xTurn = true;
-                game.checkWin();
-            }}
-        else{
-
+    
+        if (botMatch && xTurn) {
             game.placeX(row, col);
             clickedTile.classList.add("xIcon");
             game.checkWin();
-            randomBotMove();
+    
+            setTimeout(() => {
+                randomBotMove();
+                game.checkWin();
+            }, 100);
+        } else {
+            if (xTurn) {
+                game.placeX(row, col);
+                clickedTile.classList.add("xIcon");
+            } else {
+                game.placeO(row, col);
+                clickedTile.classList.add("oIcon");
+            }
+            xTurn = !xTurn; // Toggle player's turn
             game.checkWin();
-
         }
     };
 
-
-    const start = () =>{
-         xTurn = true;
+    const start = () => {
+        xTurn = true;
         tiles.forEach((tile) => {
-            tile.addEventListener("click", clickHandler,{once:true});
+            tile.addEventListener("click", clickHandler, { once: true });
         });
         tiles.forEach((tile) => {
             tile.classList.remove("xIcon", "oIcon");
         });
-    
-    }
-    
+    };
+
     const restart = () => {
         game.clearBoard();
         start();
-    }
+    };
 
-    const restarBtn = document.querySelector(".restartBtn")
+    const restartBtn = document.querySelector(".restartBtn");
 
-    restarBtn.addEventListener("click",restart)
+    restartBtn.addEventListener("click", restart);
 
-    
-    const switchBot = () => {
-        display.botMatch = !display.botMatch;
-    }
-
+    const toggleBot = () => {
+        botMatch = !botMatch;
+        if (botMatch && !xTurn) {
+            // If the bot is enabled and it's not the bot's turn, trigger the bot's move.
+            setTimeout(() => {
+                randomBotMove();
+                game.checkWin();
+            }, 100);
+        }
+    };
 
     const randomBotMove = () => {
         let emptySpot = [];
         for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) { // Fixed the loop to iterate through all columns
+            for (let j = 0; j < 3; j++) {
                 if (game.board[i][j] === 0) {
-                    emptySpot.push([i, j]); // Push an array [i, j] for empty spot
+                    emptySpot.push([i, j]);
                 }
             }
         }
-    
+
         if (emptySpot.length > 0) {
             const randomIndex = Math.floor(Math.random() * emptySpot.length);
-            const [row, col] = emptySpot[randomIndex]; // Destructuring the array
+            const [row, col] = emptySpot[randomIndex];
             game.placeO(row, col);
             tiles[row * 3 + col].classList.add("oIcon");
         }
     };
-    
+
     return {
         start,
-        botMatch,
-        switchBot,
-
+        toggleBot,
     };
 })();
 
 
-display.start()
 
-
-  
-
+display.start();
